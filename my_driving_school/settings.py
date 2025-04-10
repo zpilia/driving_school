cd973fc56ff2a336a5a59c1f35ca4c298d7022a2
Zoé Pilia <61697702+zpilia@users.noreply.github.com>
2025-04-10 15:41:26 +0200

    [build/tailwind] refactor: remove Tailwind setup from launcher and rename templates folder

--- my_driving_school/settings.py
+++ my_driving_school/settings.py
@@ -61,7 +61,7 @@ LOGIN_REDIRECT_URL = '/accounts/dashboard/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
