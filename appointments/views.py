a8ffe097c9c3e5309e5a0d8f47475c5157a9ee5d
Zoé Pilia <61697702+zpilia@users.noreply.github.com>
2025-04-21 22:23:35 +0200

    feat(appointments): display student appointments in ListView with custom template

--- appointments/views.py
+++ appointments/views.py
@@ -1,7 +1,15 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+from .models import Appointment
+from datetime import date
 
-class AppointmentListView(TemplateView):
+class AppointmentListView(ListView):
+    model = Appointment
     template_name = 'appointments/list.html'
+    context_object_name = 'appointments'
+
+    def get_queryset(self):
+        user = self.request.user
+        return Appointment.objects.filter(student=user).order_by("date", "time")
 
 class GeneralScheduleView(TemplateView):
     template_name = 'appointments/general_schedule.html'
