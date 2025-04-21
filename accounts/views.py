b6bbde7d0cd3c7553a5413bc13a685691800ad3c
emilie <emilie.le-lan@epitech.eu>
2025-04-21 15:09:49 +0200

    feat: import Q from django db models

--- accounts/views.py
+++ accounts/views.py
@@ -20,6 +20,7 @@ from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.urls import reverse
 from django.conf import settings
+from django.db.models import Q
 
 # -------------------------------
 # DASHBOARD VIEWS PAR ROLE
