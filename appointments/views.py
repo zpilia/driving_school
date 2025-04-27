2342d68282fb812be7306cabf87984f09de05d2f
Zoé Pilia <61697702+zpilia@users.noreply.github.com>
2025-04-27 18:14:07 +0200

    refactor: planning schedule: clean Appointment model, implement filtered ScheduleListView with styled Tailwind template and pagination

--- appointments/views.py
+++ appointments/views.py
@@ -1,15 +1,7 @@
-from django.views.generic import ListView, TemplateView
-from .models import Appointment
-from datetime import date
+from django.views.generic import TemplateView
 
-class AppointmentListView(ListView):
-    model = Appointment
+class AppointmentListView(TemplateView):
     template_name = 'appointments/list.html'
-    context_object_name = 'appointments'
-
-    def get_queryset(self):
-        user = self.request.user
-        return Appointment.objects.filter(student=user).order_by("date", "time")
 
 class GeneralScheduleView(TemplateView):
     template_name = 'appointments/general_schedule.html'
