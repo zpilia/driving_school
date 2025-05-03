904b83869ac085b44ef61115697c957ad0e3b6a9
emilie <emilie.le-lan@epitech.eu>
2025-05-03 15:26:52 +0200

    feat(add-hours): add paid_hours and unpaid_hours in the lesson package model

--- lessonpackages/models.py
+++ lessonpackages/models.py
@@ -9,9 +9,16 @@ class LessonPackage(models.Model):
     )
     total_hours = models.FloatField(default=0)
     used_hours = models.FloatField(default=0)
+    paid_hours = models.FloatField(default=0)
+    unpaid_hours = models.FloatField(default=0)
 
     def __str__(self):
         return f"{self.student.username} - {self.total_hours}h totales, {self.used_hours}h utilisées"
 
     def remaining_hours(self):
         return self.total_hours - self.used_hours
+
+    def save(self, *args, **kwargs):
+        self.total_hours = self.paid_hours + self.unpaid_hours
+        super().save(*args, **kwargs)
+
