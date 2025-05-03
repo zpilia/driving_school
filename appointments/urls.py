e5dd3052c1b0b9e59b72fe8c291ce6c00c1206e6
emilie <emilie.le-lan@epitech.eu>
2025-05-03 20:28:35 +0200

    feat(appointment): add url for CRUD appointments

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import AppointmentListView, GeneralScheduleView, AppointmentRequestCreateView, AppointmentRequestUpdateView, \
-    AppointmentRequestListView
+from .views import (AppointmentListView,
+                    GeneralScheduleView,
+                    AppointmentRequestCreateView,
+                    AppointmentRequestUpdateView, \
+                    AppointmentRequestListView,
+                    AppointmentCreateView,
+                    AppointmentUpdateView,
+                    AppointmentDeleteView,
+                    AppointmentView
+                    )
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -8,4 +16,9 @@ urlpatterns = [
     path('general/', GeneralScheduleView.as_view(), name='general_schedule'),
     path('request/', AppointmentRequestCreateView.as_view(), name='request_appointment'),
     path('request/<int:pk>/update/', AppointmentRequestUpdateView.as_view(), name='request_update'),
+    path('create/', AppointmentCreateView.as_view(), name='create_appointment'),
+    path('<int:pk>/edit/', AppointmentUpdateView.as_view(), name='edit_appointment'),
+    path('<int:pk>/delete/', AppointmentDeleteView.as_view(), name='delete_appointment'),
+    path('manage/', AppointmentView.as_view(), name='manage'),
+
 ]
