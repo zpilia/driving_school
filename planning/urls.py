2a9ca3963b6c947d491104d57a1de6a077291acf
emilie <emilie.le-lan@epitech.eu>
2025-05-04 12:28:32 +0200

    refactor(planning-appointment): move the planning logic from appointment package to planning package

--- planning/urls.py
+++ planning/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ScheduleListView
+from .views import ScheduleListView, general_schedule, instructor_schedule
 
 urlpatterns = [
     path('my_schedule/', ScheduleListView.as_view(), name='schedule'),
+    path('general/', general_schedule, name='general_schedule_default'),
+    path('general/<str:start_date>/', general_schedule, name='general_schedule'),
+    path('instructor/<int:instructor_id>/', instructor_schedule, name='instructor_schedule'),
 ]
